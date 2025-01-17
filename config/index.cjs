432901a6cfb7cca8dc5872a1b11be48aa4b730a5
diaozhenguo <111422705+diaozhenguo@users.noreply.github.com>
2025-01-17 09:41:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,22 +8,22 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxfe9f888755976af3',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'fa92afbdbd8f7b0b468786377d6b65a4',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '上海',
+  CITY: '上海',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小小贺',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o_a5K7GzFCKxQYen0M5Uyj-mQU-M',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'h7VQ9A8byq1nKAyHG4azvdBUqZQoaABxcdYgqa4lpi8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'WxpnybaepBOTvFZ2ZUCdiqheXJ5YJqv44etx6Nhtyz8',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '蜗牛',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: '_a5K7PRBkbGgEp-T-8axZ4ctl9k',
     }
   ],
 
